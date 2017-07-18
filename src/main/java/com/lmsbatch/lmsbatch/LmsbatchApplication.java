bc50de2f562c8177813655eed59b533b38e7e6b1
Chakra Bhandari <chakrabhandari.pp@gmail.com>
2017-07-19 08:00:57 +1000

    initial commit

---
+++ src/main/java/com/lmsbatch/lmsbatch/LmsbatchApplication.java
@@ -0,0 +1,12 @@
+package com.lmsbatch.lmsbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsbatchApplication.class, args);
+	}
+}
